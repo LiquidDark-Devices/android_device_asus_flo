b54a201a039836f423734510570cc2541cc79b2f
Josh Chasky <jchasky@gmail.com>
2016-01-02 08:20:17 -0500

    Flo: Change to Enforcing SELinux
    Change-Id: I34faf37241b7375779d2deefdaa14a777b9723dd

--- device-common.mk
+++ device-common.mk
@@ -287,6 +287,7 @@ PRODUCT_PACKAGES += \
 
 # set default USB configuration
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    sys.usb.config=mtp,adb \
     persist.sys.usb.config=mtp,adb \
     ro.adb.secure=0 \
     ro.du.updater=flo
